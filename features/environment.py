948d61f0a242ad7c6696dacf85670bf700617622
BurhanH <urazalinov@gmail.com>
2018-10-07 20:27:14 -0500

    Extended config for browser. Added a logic for Chrome browser.

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,36 @@
+from contextlib import contextmanager
 from selenium import webdriver
 WAIT_IMPL = 10
+WINDOW_SIZE = 1280, 1024
+
+
+@contextmanager
+def _create_browser(context):
+    yield
+    context.webdriver.implicitly_wait(WAIT_IMPL)
+    context.webdriver.set_window_size(WINDOW_SIZE[0], WINDOW_SIZE[1])
 
 
 def _create_firefox(context):
+    with _create_browser(context):
         context.webdriver = webdriver.Firefox()
-        context.webdriver.implicitly_wait(WAIT_IMPL)
+
+
+def _create_chrome(context):
+    with _create_browser(context):
+        context.webdriver = webdriver.Chrome()
 
 
 def _close_webdriver(context):
-    if 'webdriver' in context:
+    if "webdriver" in context:
         context.webdriver.close()
 
 
 def before_tag(context, tag):
     if tag == "firefox":
         _create_firefox(context)
+    if tag == "chrome":
+        _create_chrome(context)
 
 
 def before_all(context):
