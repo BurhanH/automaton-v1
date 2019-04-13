e6f85544aca09521dc0935b50271bb0ad17c7e96
BurhanH <urazalinov@gmail.com>
2019-04-13 12:45:03 -0500

    Added timeout after each action

--- features/steps/google.py
+++ features/steps/google.py
@@ -1,4 +1,9 @@
+import time
+
 from behave import *
+
+# It's a bad practice
+TIMEOUT = 1  # in sec
 
 
 @when('user goes to google')
@@ -14,15 +19,18 @@ def enter_term_and_click_search_button(context, target_text):
     search_textfield.is_displayed()
     search_textfield.is_enabled()
     search_textfield.send_keys(target_text)
+    time.sleep(TIMEOUT)
     # Searching and waiting the search drop-down menu
     menu = context.webdriver.find_element_by_css_selector('.FPdoLc.VlcLAe')
     menu.is_displayed()
     menu.is_enabled()
+    time.time(TIMEOUT)
     # Clicking Google Search button
     search_btn = context.webdriver.find_element_by_css_selector("input[name='btnK']")
     search_btn.is_displayed()
     search_btn.is_enabled()
     search_btn.click()
+    time.sleep(TIMEOUT)
     # Verifying search results
     context.webdriver.find_element_by_css_selector('.bNg8Rb').is_displayed()
     # TODO Needs to add more verification/s
