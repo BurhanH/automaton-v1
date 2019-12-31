d1914d86f694f4533dc38df54fb913bd8846f540
Baur Urazalinov <burazalinov@gmail.com>
2019-12-30 20:58:37 -0600

    Increased timeout. Made test more robust

--- features/steps/google.py
+++ features/steps/google.py
@@ -1,9 +1,10 @@
 import time
 
 from behave import *
+from selenium.webdriver.common.keys import Keys
 
 # It's a bad practice
-TIMEOUT = 1  # in sec
+TIMEOUT = 2  # in sec
 
 
 @when('user goes to google')
@@ -18,18 +19,7 @@ def enter_term_and_click_search_button(context, target_text: str) -> None:
     search_textfield = context.webdriver.find_element_by_name('q')
     search_textfield.is_displayed()
     search_textfield.is_enabled()
-    search_textfield.send_keys(target_text)
-    time.sleep(TIMEOUT)
-    # Searching and waiting the search drop-down menu
-    menu = context.webdriver.find_element_by_css_selector('.FPdoLc.VlcLAe')
-    menu.is_displayed()
-    menu.is_enabled()
-    time.sleep(TIMEOUT)
-    # Clicking Google Search button
-    search_btn = context.webdriver.find_element_by_css_selector("input[name='btnK']")
-    search_btn.is_displayed()
-    search_btn.is_enabled()
-    search_btn.click()
+    search_textfield.send_keys(target_text + Keys.ENTER)
     time.sleep(TIMEOUT)
     # Verifying search results
     context.webdriver.find_element_by_css_selector('.bNg8Rb').is_displayed()
